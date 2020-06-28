4c48a5d58005f4c2d21e32dc47cb22c040227f61
gtblackmore <gtblackmore@gmail.com>
2020-06-28 00:34:00 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString) {
+  const timeHour = parseInt(timeString)
+
+  if (timeHour < "12") {
+    return "Good Morning"
+  }
+  else if (timeHour < "17") {
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message;
+}
